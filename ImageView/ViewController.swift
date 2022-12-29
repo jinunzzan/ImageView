841d05609c7a2753898fe5cd1f70bf1b827945b7
jinunzzan <lovebox6607@gmail.com>
2022-12-29 17:30:50 +0900

    스위프트 프로그래밍 책 - 원하는 이미지 화면에 출력하기

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var btnResize: UIButton!
+    
+    var isZoon = false
+    var imgOn : UIImage?
+    var imgOff : UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+     
+        imgOn = UIImage(named: "on.jpg")
+        imgOff = UIImage(named: "off.jpg")
+        
+        imgView.image = imgOn
     }
 
 
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+    }
 }
 
