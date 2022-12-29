24dfab335a25ddad45021684537bc625230df3db
jinunzzan <lovebox6607@gmail.com>
2022-12-29 17:44:44 +0900

    버튼으로 이미지 확대, 축소하기 스위치로 이미지 변경하기

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var btnResize: UIButton!
     
-    var isZoon = false
+    var isZoom = false
     var imgOn : UIImage?
     var imgOff : UIImage?
     
@@ -28,9 +28,27 @@ class ViewController: UIViewController {
 
 
     @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale: CGFloat = 2.0
+        var newWidth: CGFloat, newHeight: CGFloat
+        if (isZoom) {
+            newWidth = imgView.frame.width / scale
+            newHeight = imgView.frame.height / scale
+            btnResize.setTitle("확대", for: .normal)
+        } else {
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
     }
     
     @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
     }
 }
 
